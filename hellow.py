f930be781586a7b98a7dec1ed7096d87726245d3
nahar <nahar.a@husky.neu.edu>
2024-01-19 12:53:46 -0800

    hello world in python

---
+++ hellow.py
@@ -0,0 +1,18 @@
+# asking the user for their name
+# & clean and capitalize via chained
+name = input("whats your name? ").strip().title()
+
+# say hi to user (First name)
+# the f makes it formated it properly accepting the inserted variable string
+print(f"hello, {name}")
+
+
+
+
+# split user name into first and last
+#first, last = name.split(" ")
+# clean input, ie remove whitespace
+#name.strip()
+#capitalize input
+#name.capitalize() -- only does first letter
+#name.title() 
